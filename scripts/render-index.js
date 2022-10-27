b07d982a784d08c8b8dd260d7d3b46015e869c04
Ge11ert <gellert94@mail.ru>
2022-10-27 23:03:22 +0300

    Add github deploy workflow

--- scripts/render-index.js
+++ scripts/render-index.js
@@ -2,6 +2,7 @@ const pug = require('pug');
 const path = require('path');
 
 const PATH_PREFIX = 'https://github.com/funbox/api-blueprint-tutorial/tree/master/examples';
+const EXAMPLE_PREFIX = process.env.BASE_PATH || '';
 
 const baseDir = path.resolve(__dirname, '../');
 const indexFile = path.join(baseDir, 'src/index.pug');
@@ -9,7 +10,7 @@ const indexFile = path.join(baseDir, 'src/index.pug');
 function renderIndexFile(examples) {
   const extendedExamples = examples.map(exmp => ({
     name: exmp,
-    to: `/examples/${exmp.replace(/(.+)\.apib/, '$1.html')}`,
+    to: `${EXAMPLE_PREFIX}/examples/${exmp.replace(/(.+)\.apib/, '$1.html')}`,
     source: `${PATH_PREFIX}/${exmp}`,
   }));
 
